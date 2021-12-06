526c4bee9e44b7fa3633a3edf227805761c2e876
KimSatan <47294996+KimSatan@users.noreply.github.com>
2021-12-06 15:00:52 +0800

    commit

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -13,6 +13,7 @@ class Description extends React.Component {
             >
                     <Text m={{ l: "7px" }}>
                     Dumb Donuts is a collection of NFTs (unique digital collectibles) based on the works of CryptoPunks and Weird Whales. 10,000 donuts have been generated from a buffet of combinations, each with unique traits.<br></br><br></br>
+                    For rarity table, click <Anchor href="https://docs.google.com/spreadsheets/d/1iKwPru5wzh3CkW5wyeAPBFzvzdQffY8RPdvbAKFHyp8/edit?usp=sharing" target="_blank" d="inline-block">here</Anchor>.<br></br><br></br>
                     Treat yourself to a dumb donut today at only 10 MATIC each!
                     </Text>
             </Div>
