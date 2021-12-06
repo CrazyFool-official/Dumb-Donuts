8bd8f5ce0ee2efd180f557d088dddf1029c14476
KimSatan <47294996+KimSatan@users.noreply.github.com>
2021-12-06 15:24:00 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
               <Div w="800px">
                 <Text textSize="title" textWeight="500" m={{ l: "7px" }}>About Crazy Fool</Text>
                 <Text m={{ l: "7px" }}>Crazy Fool was created with the sole purpose of giving back to society, helping those in need and raising awareness of various matters in the world.
-                With that purpose in mind, **60% of all Crazy Fool collection inital sales** will be donated to various organisations such as orphanages, charities and etc. Let us do our part to make the world a better place!</Text>
+                With that purpose in mind, <b>60% of all Crazy Fool collection inital sales</b> will be donated to various organisations such as orphanages, charities and etc. Let us do our part to make the world a better place!</Text>
               </Div>
             </Div>
           )
