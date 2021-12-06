69e575baacda173766d59371bfc309da13fdd0bd
KimSatan <47294996+KimSatan@users.noreply.github.com>
2021-12-06 11:55:37 +0800

    commit

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -4,8 +4,8 @@ import gif1 from './gif/1.gif'
 import gif2 from './gif/2.gif'
 import gif3 from './gif/3.gif'
 import gif4 from './gif/4.gif'
-import gif5 from './gif/5.gif'
-import gif6 from './gif/6.gif'
+// import gif5 from './gif/5.gif'
+// import gif6 from './gif/6.gif'
 
 
 class GIF extends React.Component {
@@ -18,12 +18,10 @@ class GIF extends React.Component {
                 m={{ b: "40px" }}
             >
                 <Col d="flex" flexDir="row">
-                    <Image src={gif1} rounded="xl" m={{ r: "40px" }} />
-                    <Image src={gif2} rounded="xl" m={{ r: "40px" }} />
-                    <Image src={gif3} rounded="xl" m={{ r: "40px" }} />
-                    <Image src={gif4} rounded="xl" m={{ r: "40px" }} />
-                    <Image src={gif5} rounded="xl" m={{ r: "40px" }} />
-                    <Image src={gif6} rounded="xl" m={{ r: "40px" }} />
+                    <Image src={gif1} rounded="xl" m={{ r: "50px" }} />
+                    <Image src={gif2} rounded="xl" m={{ r: "50px" }} />
+                    <Image src={gif3} rounded="xl" m={{ r: "50px" }} />
+                    <Image src={gif4} rounded="xl" m={{ r: "50px" }} />
                 </Col>
             </Div>
             )
